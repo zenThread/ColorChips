cbdfa8426cfc5473455c4f072fdc7bd9d6c51242
zenThread <isaiahbchilds@gmail.com>
2024-04-24 21:02:42 -0400

    Tested edge cases and refined clean up.

--- Program.cs
+++ Program.cs
@@ -20,7 +20,17 @@ namespace ChipSecuritySystem
 
 
         };
+        public static List<ColorChip> ExampleChips1 = new List<ColorChip>() {
 
+        new ColorChip(Color.Blue,Color.Yellow),
+        new ColorChip(Color.Red,Color.Purple),
+        new ColorChip(Color.Yellow,Color.Red),
+        new ColorChip(Color.Yellow,Color.Red),
+        new ColorChip(Color.Orange,Color.Purple),
+
+
+
+        };
 
         public static void Main(string[] args)
         {
@@ -28,11 +38,10 @@ namespace ChipSecuritySystem
            var res = GetChipSequence(ExampleChips);
             Console.WriteLine(res ==null ? Constants.ErrorMessage: string.Join(" || ",res.Select(x => x.ToString())));
 
+            res = GetChipSequence(ExampleChips1);
+            Console.WriteLine(res == null ? Constants.ErrorMessage : string.Join(" || ", res.Select(x => x.ToString())));
+
         }
-
-
- 
-
 
         public static List<ColorChip> GetChipSequence( List<ColorChip> c)
         {
@@ -54,17 +63,19 @@ namespace ChipSecuritySystem
                     endcolor = currC.EndColor;
                     currC = c.Where(n => n != seq[0]).ToList().GetNextSequence(endcolor.Value);
                 }
-                seq.Add(currC);
-                return seq.Where(o => o!= null).ToList();
+                if (currC != null)
+                {
+                    seq.Add(currC);
+                }
+                var targetSeq = seq.Where(o => o != null && seq.LastOrDefault().EndColor ==Color.Green).ToList();
+                return targetSeq.Count == 0 ? null : targetSeq;
+
             }
-            else {
+            else 
+            {
             return null;
             }
-
-
         }
-
-
 
 
 
